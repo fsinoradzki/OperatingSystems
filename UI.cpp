8a745937b3d0dcfa82ffc875210b71be2fb35e23
fsinoradzki <fsinoradzki@gmail.com>
2017-05-02 13:53:04 -0400

    Add files via upload
    New UI/driver

--- UI.cpp
+++ UI.cpp
@@ -6,75 +6,71 @@
 using namespace std;
 
 
-UI::UI(List Lis, Directory Dir){
-	D = Dir;
-	L = Lis;
-	cout << "Costructed the UI with the List and Dir\n";
+UI::UI(){
+	instruction = "";
+	filename = "";
+}
+
+string UI::getInstruction(){
+	return instruction;
+}
+
+string UI::getFilename(){
+	return filename;
 }
                   
-void UI::Edit(string filename){
-	cout<<"Let's edit yo\n";
-}    
-
-void UI::Create(string filename){
-	cout<<"CREATAN\n";
-}
-
-void UI::Delete(string filename){
-	cout<<"delet this\n";
-}  
-                 
-void UI::Type(string filename){
-	cout<<"You're just my type ;)\n";
-}  
-
-void UI::Dir(){
-	D.add_file_to_dir("file1");
-    D.add_file_to_dir("file2");
-    D.add_file_to_dir("file3");
-	D.show_all();
-}
-
-void UI::ExecInstruction(string instruction, string filename){
-	cout <<instruction<<endl;
-	cout <<filename<<endl;
-	if(instruction == "edit")
-		Edit(filename);
-	else if(instruction == "create")
-		Create(filename);
-	else if(instruction == "delete") 
-		Delete(filename);
-	else if(instruction == "type")   
-		Type(filename);
-	else if(instruction =="dir")    
-		Dir(); //Can possibly fudge up if there's no filename
-}
+//void UI::Edit(string fname){
+//	cout<<"Let's edit yo\n";
+//}    
+//
+//void UI::Create(string fname){
+//	cout<<"CREATAN\n";
+//}
+//
+//void UI::Delete(string fname){
+//	cout<<"delet this\n";
+//}  
+//                 
+//void UI::Type(string fname){
+//	cout<<"You're just my type ;)\n";
+//}  
+//
+//void UI::Dir(){
+//	
+//}
+//
+//void UI::ExecInstruction(string instr, string fname){
+//	cout <<instruction<<endl;
+//	cout <<filename<<endl;
+//	if(instruction == "edit")
+//		Edit(filename);
+//	else if(instruction == "create")
+//		Create(filename);
+//	else if(instruction == "delete") 
+//		Delete(filename);
+//	else if(instruction == "type")   
+//		Type(filename);
+//	else if(instruction =="dir")    
+//		Dir(); //Can possibly fudge up if there's no filename
+//}
 
 void UI::Start(){
 	bool exit = false;
-	string str = "", instr, fname;
+	string str = "";
 	int pos;
 	
-	//Uses a while loop to loop through inputs
 	//If the input is null, nothing will execute
 	//If there is input, split up the instruction and argument
 	//Then exit if exit is the instruction, or execute the instruction if run
-	while(exit == false){
-		cout << "$";
-		getline(cin, str);
-		
-		//Everything before the space (text) is the instruction
-		//Everything after the space  is the filename
-		pos = str.find(" ");
-		instr = str.substr(0,pos);
-		fname = str.substr(pos+1,str.length()-1);
-		
-		for(int i=0;i<instr.length();i++)
-			instr[i] = tolower(instr[i]);
-		
-		if(instr == "exit")
-			exit = true;
-		else
-			ExecInstruction(instr, fname);
-	}
+	cout << "$";
+	getline(cin, str);
+	
+	//Everything before the space (text) is the instruction
+	//Everything after the space  is the filename
+	pos = str.find(" ");
+	instruction = str.substr(0,pos);
+	filename = str.substr(pos+1,str.length()-1);
+	
+	for(int i=0;i<instruction.length();i++)
+		instruction[i] = tolower(instruction[i]);
 }
--- driver.cpp
+++ driver.cpp
@@ -9,6 +9,50 @@
 #include "file.h"
 #include "file.cpp"
 
+Directory D;
+List L;
+                  
+void Edit(string fname){
+	cout<<"Let's edit yo\n";
+}    
+
+void Create(string fname){
+	if(D.file_exists_check(fname) == true)
+		cout<<fname<<" already exists.\n";
+	else
+		D.add_file_to_dir(fname);
+}
+
+void Delete(string fname){
+	if(D.file_exists_check(fname) == true)
+		D.delete_file_from_dir(fname);
+	else
+		cout<<fname<<" doesn't exist.\n";
+}  
+                 
+void Type(string fname){
+	cout<<"You're just my type ;)\n";
+}  
+
+void Dir(){
+	for(int i = 0; i < D.directorylength(); i++){
+		D.show_specific(i);
+		cout<<" x blocks"<<endl;
+	}
+}
+
+void ExecInstruction(string instr, string fname){
+	if(instr == "edit")
+		Edit(fname);
+	else if(instr == "create")
+		Create(fname);
+	else if(instr == "delete") 
+		Delete(fname);
+	else if(instr == "type")   
+		Type(fname);
+	else if(instr =="dir")    
+		Dir(); //Can possibly fudge up if there's no filename
+}
 
 int main(){
         //Call File System to initialize a blank list with just head
@@ -17,8 +61,15 @@ int main(){
         //Call UI to initialize the UI
         //Start the UI
 		
-		Directory D;
-		List L;
-		UI gui(D, L);
-		gui.Start();
+
+		bool keepGoing = true;
+		UI gui;
+		
+		while(keepGoing){
+			gui.Start();
+			if(gui.getInstruction() == "exit")
+				keepGoing = false;
+			else
+				ExecInstruction(gui.getInstruction(), gui.getFilename());
+		}
 }
